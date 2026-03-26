505c440bcbaa1bce031d03e3ee78292264c38b6b
Rost237 <rostangounou@gmail.com>
2026-03-26 17:25:43 +0100

    Create main.js
    pour gérer les plateforme androïde et Windows

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+  });
+
+  // Charge ton build Vite
+  win.loadFile(path.join(__dirname, 'dist/index.html'));
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
